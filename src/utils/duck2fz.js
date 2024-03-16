d680980ef08470487b639103bad4154f4d0bfed8
Esteban Fuentealba <efuentealba@json.cl>
2024-03-16 01:06:12 -0300

    Add Scape to `'`

--- src/utils/duck2fz.js
+++ src/utils/duck2fz.js
@@ -62,7 +62,7 @@ badusb.quit();
         const command = parts[0];
         const args = parts.slice(1).join(' ');
         if (command in command_mapping) {
-            const js_command = command_mapping[command].replace('{}', args);
+            const js_command = command_mapping[command].replace('{}', args).replace(/'/g, "\'");
             jsCode += `\t${js_command}\n`;
         } else {
             console.warn(`Warning: Unsupported DuckyScript command '${command}'`);
